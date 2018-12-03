b7dd27707f114498370e1f9617c5de5a50073ba4
Feng <438931118@qq.com>
2018-12-03 14:47:41 +0800

    chapter 3:Custom error pages(3c)

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
