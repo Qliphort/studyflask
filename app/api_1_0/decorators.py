6f20e2ef8688c45e089338a2239e98df928fbc97
Qliphort <jijinyu_438931118@qq.com>
2018-12-18 17:22:30 +0800

    fix some bug

--- app/api_1_0/decorators.py
+++ app/api_1_0/decorators.py
@@ -6,9 +6,9 @@ from .errors import forbidden
 def permission_required(permission):
     def decorator(f):
         @wraps(f)
-        def decorated_funtion(*args, **kwargs):
+        def decorated_function(*args, **kwargs):
             if not g.current_user.can(permission):
                 return forbidden('Insufficient permissions')
             return f(*args, **kwargs)
-        return decorated_funtion
+        return decorated_function
     return decorator
